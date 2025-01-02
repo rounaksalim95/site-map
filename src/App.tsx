d5c041a61f64195ed3755659eb66ba822f2660a9
rounaksalim95 <rounaksalim@gmail.com>
2025-01-01 21:03:15 -0800

    One more try, looks more promising...

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
 import './App.css';
 import BlogsPage from './components/Blog/BlogsPage';
 import ThemeToggle from './components/ThemeToggle';
 import { ThemeProvider } from './context/ThemeContext';
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const redirectFrom404 = sessionStorage.getItem('redirect_from_404');
+    if (redirectFrom404) {
+      navigate(redirectFrom404);
+      sessionStorage.removeItem('redirect_from_404');
+    }
+  }, [navigate]);
+
   return (
     <ThemeProvider>
       <Router>
