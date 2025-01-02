760d18df05b1d428d29baddf31b771addcd5466c
rounaksalim95 <rounaksalim@gmail.com>
2025-01-02 03:44:54 -0800

    Remove arrows from input text fields

--- src/components/RandomTidBits/BuyVsRentCalculator.tsx
+++ src/components/RandomTidBits/BuyVsRentCalculator.tsx
@@ -58,7 +58,7 @@ const InputField: React.FC<InputFieldProps> = ({
                 type="number"
                 value={value}
                 onChange={(e) => onChange(Number(e.target.value))}
-                className="border rounded-md px-3 py-2 bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 border-gray-300 dark:border-gray-600"
+                className="border rounded-md px-3 py-2 bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 border-gray-300 dark:border-gray-600 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
                 min={min}
                 max={max}
                 step={step}
